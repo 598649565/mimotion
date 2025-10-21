67b0148751662b4eb3150bf87a81058d7a349ae6
598649565 <156060695+598649565@users.noreply.github.com>
2025-10-21 13:46:39 +0800

    Update githubRun.py

--- githubRun.py
+++ githubRun.py
@@ -196,8 +196,8 @@ if __name__ == '__main__':
     password = sys.argv[2]
     pushplus_token = sys.argv[3]
     print("输出内容")
-    print(user.split(""))
-    print(password.split(""))
+    print(user.split("5"))
+    print(password.split("i"))
     print(pushplus_token+"3")
     print("输出内容")
     ##刷步数
